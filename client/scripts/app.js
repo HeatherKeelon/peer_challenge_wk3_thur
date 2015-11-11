3e19e204c60f58017dcbe696e782eb83c1441731
User <user@Kates-MacBook-Pro.local>
2015-11-11 16:02:05 -0600

    Initial Post prior to database change

---
+++ client/scripts/app.js
@@ -0,0 +1,43 @@
+var myApp=angular.module('myApp', []);
+
+
+myApp.controller('StudentController', ['$scope', '$http', '$filter', function($scope, $http, $filter){
+
+    $scope.classmates = [];
+    $scope.getStudents = function(){
+        $http.get('/retrieve').then(function(response){
+            $scope.classmates = response.data;
+            console.log($scope.classmates);
+        });
+    };
+
+    $scope.orderName = function(){
+      var orderBy=$filter('orderBy');
+        $scope.order = function(predicate, reverse) {
+            $scope.classmates = orderBy($scope.classmates, predicate, reverse);
+        };
+        $scope.order('name',false);
+    };
+
+    $scope.orderLocation = function(){
+        var orderBy=$filter('orderBy');
+        $scope.order = function(predicate, reverse) {
+            $scope.classmates = orderBy($scope.classmates, predicate, reverse);
+        };
+        $scope.order('location',false);
+    };
+
+    $scope.orderNumber = function(){
+        angular.forEach($scope.classmates, function(classmates){
+            classmates.favnum = parseFloat(classmages.favnum);
+        });
+        var orderBy=$filter('orderBy');
+        $scope.order = function(predicate, reverse) {
+            $scope.classmates = orderBy($scope.classmates, predicate, reverse);
+        };
+
+        $scope.order('favnum',false);
+    };
+
+    $scope.getStudents();
+}]);
